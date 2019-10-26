c8ea48d5239ceed1ce6223f1ec3520c258a3c9a0
PabloHNieto <pablo.h.nieto@gmail.com>
2019-10-26 17:25:08 +0200

    Updating CRUD operations to users and tasks

--- blueprints/task.py
+++ blueprints/task.py
@@ -1,25 +1,29 @@
 from flask import Blueprint, request, jsonify
+import json
 from models.Task import Tasks
+from middlewares.authentication import login_required
 
 task_page = Blueprint("tasks", __name__, url_prefix="/tasks")
 
 @task_page.route("", methods=["GET","POST"])
+@login_required
 def get_tasks():
   if request.method == "GET":
     return Tasks.objects.to_json() 
 
   if request.method == "POST":
-    new_task = Tasks(**request.get_json())
+    new_task = Tasks(**request.get_json(), owner=request.user.id)
     try:
       new_task.save()
-      return new_task.to_json()
+      return jsonify({"task": json.loads(new_task.to_json())})
     except Exception  as e:
       return jsonify({"error": str(e)})
 
 @task_page.route("/<taskID>", methods=["GET","PATCH", "DELETE"])
+@login_required
 def handle_task_by_id(taskID):
   try:
-    task = Tasks.objects(id=taskID).as_pymongo()
+    task = Tasks.objects(id=taskID, owner=request.user.pk).as_pymongo()
     if len(task) == 0:
       raise Exception("Task not found")
 
--- blueprints/user.py
+++ blueprints/user.py
@@ -1,22 +1,20 @@
+import json
 from flask import Blueprint, request, jsonify
 from models.User import Users
-import json
+from middlewares.authentication import login_required
 
 user_page = Blueprint("users", __name__, url_prefix="/users")
 
-# @user_page.route("/", defaults={"page": "index"})
-@user_page.route("", methods=["GET", "POST"])
+@user_page.route("", methods=["POST"])
 def handle_users():
-  if request.method == "GET":
-    return Users.objects.to_json()
-  
   if request.method == "POST":
     data = request.get_json()
     try:
       newUser = Users(**data)
       changed_fields = list(data.keys())
       newUser.save(signal_kwargs={"changed_fields":changed_fields})
-      return newUser.to_json()
+      token = newUser.generate_auth_token()
+      return jsonify({"user:": json.loads(newUser.to_json()), "token": token})
     except Exception  as e:
       return jsonify({"error": str(e)})
 
@@ -31,25 +29,44 @@ def login_user():
     except Exception as e:
       return jsonify({"error": str(e)}), 500
 
-@user_page.route("/<userID>", methods=["GET", "PATCH", "DELETE"])
-def handle_user_by_id(userID):
-  
+@user_page.route("/logout", methods=["POST"])
+@login_required
+def logout():
   try:
-    user = Users.objects(id=userID).as_pymongo()
+    if request.method == "POST":
+      request.user.remove_auth_token(request.token)
+      return user.to_json()
+  except Exception as e:
+    return jsonify({"error": str(e)})
+
+@user_page.route("/logoutall", methods=["POST"])
+@login_required
+def logoutall():
+  try:
+    if request.method == "POST":
+      request.user.remove_all_auth_token()
+      return request.user.to_json()
+  except Exception as e:
+    return jsonify({"error": str(e)})
+
+@user_page.route("/me", methods=["GET", "PATCH", "DELETE"])
+@login_required
+def handle_user_by_id():
+  try:
+    user = request.user
     if len(user) == 0:
       raise Exception("User not found")
 
     if request.method == "GET":
-      return user.to_json()
-
+      return jsonify(json.loads(user.to_json()))
     elif request.method == "PATCH":
       data = request.get_json()
-      user.modify(**data)
-      return user.to_json()
-
+      user.update(**data)
+      changed_fields = list(data.keys())
+      user.save(signal_kwargs={"changed_fields":changed_fields})
+      return jsonify(json.loads(user.to_json()))
     elif request.method == "DELETE":
       user.delete()
-      return "Deleted user"
-
+      return jsonify(json.loads(user.to_json()))
   except Exception as e:
     return jsonify({"error": str(e)})
