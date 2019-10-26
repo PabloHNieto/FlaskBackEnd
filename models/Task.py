f4f05b759ea4cded9d4909dccc53e3105367084b
PabloHNieto <pablo.h.nieto@gmail.com>
2019-10-26 17:24:35 +0200

    Adding User reference and in cascade delete

--- models/Task.py
+++ models/Task.py
@@ -1,6 +1,8 @@
-from mongoengine import Document, StringField, BooleanField
+from mongoengine import Document, StringField, BooleanField, ReferenceField
+from models.User import Users
+import json
 
 class Tasks(Document):
   completed = BooleanField(required=True, default=False)
   description = StringField(min_length=7, required=True)
-    
+  owner = ReferenceField(Users, reverse_delete_rule=2, required=True)
